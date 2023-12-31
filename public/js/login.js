1895f3c390f77e2b1017709e7adfeb451583ab3b
Richard Song <rsong1993@gmail.com>
2023-12-30 16:28:34 -0800

    Updated login.js to handle user input retrieval. Added function to send a POST request to /login via axios

---
+++ public/js/login.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('login.js loaded successfully!');
+
+    const registerBtn = document.querySelector('#registerBtn');
+    const loginBtn = document.querySelector('#loginBtn');
+
+    const handleRegister = async function() {
+        const username = document.querySelector('#registerUsername').value;
+        const password = document.querySelector('#registerPassword').value;
+        try {
+            await axios.post('/login', { username, password });
+            console.log('User registered successfully!');
+        } catch (error) {
+            console.error(error);
+        };
+    };
+
+    const handleLogin = async function() {
+        const username = document.querySelector('#loginUsername').value;
+        const password = document.querySelector('#loginPassword').value;
+        try {
+            const response = await axios.post('/login', { username, password });
+            //Debug
+            console.log(username);
+            console.log(password);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    if (registerBtn) {
+        registerBtn.addEventListener('click', handleRegister);
+    };
+
+    if (loginBtn) {
+        loginBtn.addEventListener('click', handleLogin);
+    };
+});
