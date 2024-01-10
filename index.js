4e5006844bda142d5cb800a4a3d7f16cddb8cae1
Richard Song <rsong1993@gmail.com>
2024-01-09 23:29:53 -0800

    heroku - updated port

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const exphbs = require('express-handlebars');
 
